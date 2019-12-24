f1f145960b3b934082766adbdd1ca635ed5edeac
Robert Faust <rob@robfaust.com>
2019-12-24 13:21:35 -0600

    Initial commit

---
+++ SuppressibleMessageBox/SuppressibleMessageBox.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Com.RobFaust.Common.UserInterface
+{
+    partial class SuppressibleMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SuppressibleMessageBox));
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.tlpMessage = new System.Windows.Forms.TableLayoutPanel();
+            this.chkDontShow = new System.Windows.Forms.CheckBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.imgIcon = new System.Windows.Forms.PictureBox();
+            this.tlpMain.SuspendLayout();
+            this.tlpMessage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 1;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.Controls.Add(this.btnOK, 0, 1);
+            this.tlpMain.Controls.Add(this.tlpMessage, 0, 0);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 2;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpMain.Size = new System.Drawing.Size(389, 140);
+            this.tlpMain.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(302, 105);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(12);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "&OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // tlpMessage
+            // 
+            this.tlpMessage.BackColor = System.Drawing.SystemColors.Window;
+            this.tlpMessage.ColumnCount = 2;
+            this.tlpMessage.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpMessage.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMessage.Controls.Add(this.chkDontShow, 1, 1);
+            this.tlpMessage.Controls.Add(this.lblMessage, 1, 0);
+            this.tlpMessage.Controls.Add(this.imgIcon, 0, 0);
+            this.tlpMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMessage.Location = new System.Drawing.Point(0, 0);
+            this.tlpMessage.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpMessage.Name = "tlpMessage";
+            this.tlpMessage.Padding = new System.Windows.Forms.Padding(12, 12, 24, 12);
+            this.tlpMessage.RowCount = 2;
+            this.tlpMessage.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMessage.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpMessage.Size = new System.Drawing.Size(389, 93);
+            this.tlpMessage.TabIndex = 1;
+            // 
+            // chkDontShow
+            // 
+            this.chkDontShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkDontShow.AutoSize = true;
+            this.chkDontShow.Location = new System.Drawing.Point(56, 61);
+            this.chkDontShow.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.chkDontShow.Name = "chkDontShow";
+            this.chkDontShow.Size = new System.Drawing.Size(172, 17);
+            this.chkDontShow.TabIndex = 0;
+            this.chkDontShow.Text = "&Don\'t show this message again";
+            this.chkDontShow.UseVisualStyleBackColor = true;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(53, 15);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(3);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(60, 13);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "lblMessage";
+            // 
+            // imgIcon
+            // 
+            this.imgIcon.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.imgIcon.Image = ((System.Drawing.Image)(resources.GetObject("imgIcon.Image")));
+            this.imgIcon.Location = new System.Drawing.Point(15, 15);
+            this.imgIcon.Name = "imgIcon";
+            this.imgIcon.Size = new System.Drawing.Size(32, 32);
+            this.imgIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imgIcon.TabIndex = 2;
+            this.imgIcon.TabStop = false;
+            // 
+            // SuppressibleMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(389, 140);
+            this.Controls.Add(this.tlpMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SuppressibleMessageBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Error";
+            this.tlpMain.ResumeLayout(false);
+            this.tlpMessage.ResumeLayout(false);
+            this.tlpMessage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TableLayoutPanel tlpMessage;
+        private System.Windows.Forms.CheckBox chkDontShow;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.PictureBox imgIcon;
+    }
+}
---
+++ SuppressibleMessageBox/SuppressibleMessageBox.cs
@@ -0,0 +1,68 @@
+﻿using SuppressibleMessageBox;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Com.RobFaust.Common.UserInterface
+{
+    public partial class SuppressibleMessageBox : Form
+    {
+        #region Constructors
+        protected SuppressibleMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        protected SuppressibleMessageBox(string message) : this()
+        {
+            lblMessage.Text = message;
+        }
+
+        protected SuppressibleMessageBox(string message, string caption) : this(message)
+        {
+            Text = caption;
+        }
+        #endregion
+
+        #region Public Properties
+        public bool Suppress => chkDontShow.Checked;
+        #endregion
+
+        #region Static Methods
+        public static DialogResult Show(string messageId, string message) => Show(messageId, message, "Error");
+
+        /// <summary>
+        /// For Program.AddUpdateAppSettings and Program.ReadSetting implementations, please visit
+        /// https://docs.microsoft.com/en-us/dotnet/api/system.configuration.configurationmanager.appsettings?view=netframework-4.8
+        /// </summary>
+        /// <param name="messageId"></param>
+        /// <param name="message"></param>
+        /// <param name="caption"></param>
+        /// <returns></returns>
+        public static DialogResult Show(string messageId, string message, string caption)
+        {
+            DialogResult result = DialogResult.None;
+            string settingId = GetSettingId(messageId);
+            bool suppress;
+            bool.TryParse(Program.ReadSetting(settingId), out suppress);
+            if (!suppress)
+            {
+                SystemSounds.Beep.Play();
+                using (SuppressibleMessageBox frm = new SuppressibleMessageBox(message, caption))
+                {
+                    result = frm.ShowDialog();
+                    if (frm.Suppress)
+                    {
+                        Program.AddUpdateAppSettings(settingId, bool.TrueString);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static string GetSettingId(string messageId)
+        {
+            return $"messages.{messageId}.hide";
+        }
+        #endregion
+    }
+}
