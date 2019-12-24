6606d4d7e9a38d6deb5d71d3efb566d1dc9f2c53
Robert Faust <rob@robfaust.com>
2019-12-24 16:17:58 -0600

    Changing to class library

--- SuppressibleMessageBox/SuppressibleMessageBox.cs
+++ SuppressibleMessageBox/SuppressibleMessageBox.cs
@@ -1,4 +1,5 @@
-﻿using SuppressibleMessageBox;
+﻿using System;
+using System.Configuration;
 using System.Media;
 using System.Windows.Forms;
 
@@ -7,13 +8,9 @@ namespace Com.RobFaust.Common.UserInterface
     public partial class SuppressibleMessageBox : Form
     {
         #region Constructors
-        protected SuppressibleMessageBox()
+        protected SuppressibleMessageBox(string message)
         {
             InitializeComponent();
-        }
-
-        protected SuppressibleMessageBox(string message) : this()
-        {
             lblMessage.Text = message;
         }
 
@@ -28,7 +25,7 @@ namespace Com.RobFaust.Common.UserInterface
         #endregion
 
         #region Static Methods
-        public static DialogResult Show(string messageId, string message) => Show(messageId, message, "Error");
+        public static DialogResult Show(Configuration config, string messageId, string message) => Show(config, messageId, message, "Error");
 
         /// <summary>
         /// For Program.AddUpdateAppSettings and Program.ReadSetting implementations, please visit
@@ -38,12 +35,12 @@ namespace Com.RobFaust.Common.UserInterface
         /// <param name="message"></param>
         /// <param name="caption"></param>
         /// <returns></returns>
-        public static DialogResult Show(string messageId, string message, string caption)
+        public static DialogResult Show(Configuration config, string messageId, string message, string caption)
         {
             DialogResult result = DialogResult.None;
             string settingId = GetSettingId(messageId);
             bool suppress;
-            bool.TryParse(Program.ReadSetting(settingId), out suppress);
+            bool.TryParse(ReadSetting(config, settingId), out suppress);
             if (!suppress)
             {
                 SystemSounds.Beep.Play();
@@ -52,7 +49,7 @@ namespace Com.RobFaust.Common.UserInterface
                     result = frm.ShowDialog();
                     if (frm.Suppress)
                     {
-                        Program.AddUpdateAppSettings(settingId, bool.TrueString);
+                        AddUpdateAppSettings(config, settingId, bool.TrueString);
                     }
                 }
             }
@@ -62,6 +59,61 @@ namespace Com.RobFaust.Common.UserInterface
         public static string GetSettingId(string messageId)
         {
             return $"messages.{messageId}.hide";
+        }
+        #endregion
+
+        #region Configuration Methods
+        private static string ReadSetting(Configuration config, string key)
+        {
+            try
+            {
+                AppSettingsSection section = config.GetSection("appSettings") as AppSettingsSection;
+                if (section != null)
+                {
+                    KeyValueConfigurationCollection appSettings = section.Settings;
+                    if (appSettings[key] != null)
+                    {
+                        return appSettings[key].Value;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+            return string.Empty;
+        }
+
+        private static bool AddUpdateAppSettings(Configuration config, string key, string value)
+        {
+            try
+            {
+                AppSettingsSection section = config.GetSection("appSettings") as AppSettingsSection;
+                if (section != null)
+                {
+                    KeyValueConfigurationCollection settings = section.Settings;
+                    if (settings[key] == null)
+                    {
+                        settings.Add(key, value);
+                    }
+                    else
+                    {
+                        settings[key].Value = value;
+                    }
+                    config.Save(ConfigurationSaveMode.Modified);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                HandleConfigError(e);
+            }
+            return false;
+        }
+
+        private static void HandleConfigError(Exception e)
+        {
+            MessageBox.Show(e.Message, "Error accessing configuration file", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
         #endregion
     }
---
+++ SuppressibleMessageBoxTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Com.RobFaust.Common.UserInterface
+{
+    static class Program
+    {
+        private static Configuration config;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string path = string.Join(Path.DirectorySeparatorChar.ToString(), Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AssemblyProduct, "settings.xml");
+            ExeConfigurationFileMap customConfigFileMap = new ExeConfigurationFileMap();
+            customConfigFileMap.ExeConfigFilename = path;
+            config = ConfigurationManager.OpenMappedExeConfiguration(customConfigFileMap, ConfigurationUserLevel.None);
+
+            // Use case #1, informational message with no response
+            SuppressibleMessageBox.Show(config, "msg1", "This message does not return a response", "Test Message #1");
+
+            // Use case #1, evaluate whether or not the message was shown
+            DialogResult result = SuppressibleMessageBox.Show(config, "msg2", "This message DOES return a response", "Test Message #2");
+            if (result == DialogResult.None)
+            {
+                MessageBox.Show("The original message was suppressed due to settings");
+            }
+            else
+            {
+                MessageBox.Show($"The message box result was \"{result.ToString()}\"");
+            }
+        }
+
+        internal static string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+    }
+}
